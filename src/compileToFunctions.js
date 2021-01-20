7437fc8cef498247f2def5315044f3cc01b4ceb8
power_840 <liuyu19921017@gmail.com>
2021-01-20 17:32:52 +0800

    feat: template compile to function

--- src/compileToFunctions.js
+++ src/compileToFunctions.js
@@ -19,8 +19,55 @@ function chars(text) {
 }
 
 function parseHTML(html) {
-  
-
+  while (html) {
+    let textEnd = html.indexOf('<');
+    if (textEnd === 0) {
+      const startTagMatch = parseStartTag();
+      if (startTagMatch) {
+        start(startTagMatch.tagName, startTagMatch.attrs);
+        continue;
+      }
+      const endTagMatch = html.match(endTag);
+      if (endTagMatch) {
+        advance(endTagMatch[0]);
+        end(endTagMatch[1]);
+        continue;
+      }
+    }
+    let text
+    if (textEnd >= 0) {
+      text = html.substring(0, textEnd)
+    }
+    if (text) {
+      advance(text.length)
+      chars(text)
+    }
+  }
+  function advance(n) {
+    html = html.substring(n);
+  }
+  function parseStartTag() {
+    const start = html.match(startTagOpen);
+    if (start) {
+      const match = {
+        tagName: start[1],
+        attrs: [],
+      };
+      advance(start[0].length);
+      let attr, end;
+      while (
+        !(end = html.match(startTagClose)) &&
+        (attr = html.match(attribute))
+      ) {
+        advance(attr[0].length);
+        match.attrs.push({ name: attr[1], value: attr[3] });
+      }
+      if (end) {
+        advance(end[0].length);
+        return match;
+      }
+    }
+  }
 }
 
 function compileToFunctions(template) {
